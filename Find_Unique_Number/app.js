807de3c31a8abed7a5437ad6e50645e5e25fa502
Bartha Hunor <bhun0r97@gmail.com>
2022-08-01 19:24:25 +0200

    find unique num challenge done

--- Find_Unique_Number/app.js
+++ Find_Unique_Number/app.js
@@ -1,16 +1,17 @@
+// DONE
 function findUniq(arr) {
   let sorted = arr.sort((a, b) => a - b);
+  let last = sorted.length - 1;
   let answ = [];
-  for (let i = 0; i <= sorted.length; i++) {
-    if (sorted[i] == sorted[i + 1]) {
-      sorted.slice(i, 1);
-    }
+  if (sorted[last] != sorted[last - 1]) {
+    answ.push(sorted[last]);
+  } else if (sorted[0] != sorted[1]) {
+    answ.push(sorted[0]);
   }
-
-  console.log(sorted);
+  return parseFloat(answ);
 }
 
-console.log(findUniq([1, 1, 1, 2, 1, 1]));
+console.log(findUniq([3, 10, 3, 3, 3]));
 // assert.strictEqual(findUniq([ 1, 0, 0 ]), 1);
 // assert.strictEqual(findUniq([ 0, 1, 0 ]), 1);
 // assert.strictEqual(findUniq([ 0, 0, 1 ]), 1);
